403928a1d4735f809e90c4babbb3ce41364f61f3
aarabian <52959180+aarabian@users.noreply.github.com>
2019-10-29 17:28:09 +0330

    1- Add Counter-Up jQuery plugin (Counter-Up is a lightweight jQuery plugin that counts up to a targeted number when the number becomes visible.) 2- Add Waypoints plugin (Waypoints is the easiest way to trigger a function when you scroll to an element.) 3- Add main.js (Thats my js file) 4- Create Counter Section with Counter up

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+jQuery(document).ready(function ($){
+    $('.web-counter-number').counterUp();
+
+}); 
+
+/* var waypoint = new Waypoint({
+    element: document.getElementById('web-counter-section'),
+    handler: function (direction) {
+        console.log('Scrolled to waypoint!')
+    }
+}); */
+
+/* jQuery(document).ready(function ($) {
+    $('.web-counter-number').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+ */

947e3a361892ec1ef2c7a72c896f2452b8180255
aarabian <52959180+aarabian@users.noreply.github.com>
2019-11-06 02:43:23 +0330

    1- Make Responsive with @media in 992px 2- Make menu fixed css & js 3- Animate in menu with Animate.css 4- Scroll to a section in menu with AnimateScroll 5- Make lightBox for gallery with fancybox 6- Add afew files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 
-jQuery(document).ready(function ($){
-    $('.web-counter-number').counterUp();
+jQuery(document).ready(function ($) {
+    /* --------- Counter Up ---------- */
+    $('.web-counter-number').counterUp({
+        delay: 10,
+        time: 1000
+    });
 
-}); 
+    /* --------- Fixed Menu ---------- */
+    var menu = $('.web-header-section');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0 && menu.hasClass('web-header-section')) {
+            menu.addClass('web-header-fixed');
+        }
+        else if ($(this).scrollTop() <= 0 && menu.hasClass('web-header-fixed')) {
+            menu.removeClass('web-header-fixed');
+        }
+    });
+});
 
 /* var waypoint = new Waypoint({
     element: document.getElementById('web-counter-section'),
